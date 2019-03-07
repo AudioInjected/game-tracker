2714c727dcbf7e7d62a86c0bba994a213d18dbf7
Rafael Batista <rafaelb24@yahoo.com>
2019-03-07 22:13:00 +0000

    create migration for genres and game_genres. create models for genres and game genres

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ActiveRecord::Base 
   belongs_to :owner
+  has_many :game_genres 
+  has_many :genres, through: :game_genres
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_215936) do
+ActiveRecord::Schema.define(version: 2019_03_07_220712) do
+
+  create_table "game_genres", force: :cascade do |t|
+    t.integer "game_id"
+    t.integer "genre_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
     t.string "owner_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "owners", force: :cascade do |t|
