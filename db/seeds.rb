03f8d28e3fa93e52442e5445205e5d24e64499ac
Rafael Batista <rafaelb24@yahoo.com>
2019-03-08 21:54:46 +0000

    Change seed data to reflect a game belongs to an owner

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ meh_sonic_fan = User.create(username: "Meh_Sonic_Fan", email: "meh@email.com", p
 rush_b = User.create(username: "Rush_B", email: "rush_b@email.com", password: "go go go")
 
 sonic_the_hedgehog = Game.create(name: "Sonic the Hedgehog", release_date: "06/23/1991", platform: "Sega Genesis")
+sonic_the_hedgehog_2nd_copy = Game.create(name: "Sonic the Hedgehog", release_date: "06/23/1991", platform: "Sega Genesis")
 counter_strike = Game.create(name: "Counter-Strike", release_date: "11/09/2000", platform: "PC")
 streets_of_rage_2 = Game.create(name: "Streets of Rage 2", release_date: "12/20/1992", platform: 
 "Sega Genesis")
@@ -14,12 +15,14 @@ beat_em_up = Genre.create(name: "Beat em Up")
 
 sonic_the_hedgehog.genres << action 
 sonic_the_hedgehog.genres << platformer 
+sonic_the_hedgehog_2nd_copy.genres << action
+sonic_the_hedgehog_2nd_copy.genres << platformer
 counter_strike.genres << fps
 streets_of_rage_2.genres << action 
 streets_of_rage_2.genres << beat_em_up
 
 rush_b.games << counter_strike
 huge_sonic_fan.games << sonic_the_hedgehog 
-huge_sonic_fan.games << streets_of_rage_2 
-meh_sonic_fan.games << sonic_the_hedgehog
+huge_sonic_fan.games << streets_of_rage_2
+meh_sonic_fan.games << sonic_the_hedgehog_2nd_copy 
 meh_sonic_fan.games << counter_strike
