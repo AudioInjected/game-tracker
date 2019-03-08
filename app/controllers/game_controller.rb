ad4fda7857395b571ecf59ca0ff3b3b3f4c0ecff
Rafael Batista <rafaelb24@yahoo.com>
2019-03-08 22:03:47 +0000

    Add route to create new game, add button to create new game in index and show page

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,8 +1,14 @@
 class GameController < ApplicationController 
 
+  get '/games/new' do 
+    redirect '/login' if !logged_in
+    erb "/games/new"
+  end 
+  
   get '/games/:id' do 
     @game = Game.find(params[:id])
     erb :'/games/show'
   end 
   
+
 end 
